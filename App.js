2600188b7e7ab0bc237fc31990b668222500379b
hochleti <hochauleticia25@gmail.com>
2021-09-30 19:35:16 +1000

    Added back firebase and bottomTabNavigation

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ import { useState } from "react";
 import { Ionicons, AntDesign } from "@expo/vector-icons";
 //import { useFormik } from "formik";
 import { Formik } from "formik";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import LoginForm from "./js/loginForm.js";
 import SignUpForm from "./js/signupForm.js";
@@ -24,7 +24,7 @@ import {
 } from "@expo-google-fonts/paytone-one";
 import { Roboto_400Regular } from "@expo-google-fonts/roboto";
 import AppLoading from "expo-app-loading";
-import background from "./assets/background.jpg";
+import background from "./assets/background.jpg"
 import Quiz from "./js/quiz.js";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -38,72 +38,6 @@ function HomeScreen({ navigation }) {
   // const response = await fetch('/api/names');
   // const names = await response.json();
 
-  // console.log(names);
-  // https://itcrowdproject.uqcloud.net/?PET_PHOTO
-  // let [fontsLoaded, error] = useFonts({
-  //   Roboto_400Regular,
-  //   PaytoneOne_400Regular,
-  // });
-  // if (!fontsLoaded) {
-  //   return <AppLoading />;
-  // }
-  // return (
-  //   <ImageBackground
-  //     source={background}
-  //     resizeMode="cover"
-  //     style={{
-  //       flex: 1,
-  //       justifyContent: "center",
-  //     }}
-  //   >
-  //     <NativeBaseProvider>
-  //       <View
-  //         style={{
-  //           alignItems: "flex-end",
-  //           justifyContent: "flex-end",
-  //           marginRight: "200px",
-  //         }}
-  //       >
-  //         <Text
-  //           color="white"
-  //           fontSize="80"
-  //           fontFamily="PaytoneOne_400Regular"
-  //           marginTop="150px"
-  //           marginRight="50px"
-  //         >
-  //           Purrrfect Pets!{" "}
-  //         </Text>
-  //         <Text color="white" fontSize="30" paddingTop="10" textAlign='center' marginRight="120px" fontFamily='PaytoneOne_400Regular' flexShrink="1">
-  //           Match with and adopt a pet {"\n"}that is most suitable for you
-  //         </Text>
-  //         <Button
-  //           mt="2em"
-  //           mraginTop="500px"
-  //           padding="25px 150px"
-  //           borderRadius="30px"
-  //           backgroundColor="#f1c737"
-  //           marginRight="150px"
-  //           fontFamily="Roboto_400Regular"
-  //           onPress={() => navigation.navigate("Login")}
-  //           title="Login In!"
-  //         >
-  //           <Text color="#545871" fontFamily='Roboto_400Regular'>Login In </Text>
-  //         </Button>
-  //         <Button
-  //           mt="2em"
-  //           padding="25px 150px"
-  //           borderRadius="30px"
-  //           marginRight="150px"
-  //           backgroundColor="#f1c737"
-  //           onPress={() => navigation.navigate("Sign Up")}
-  //           title="Sign up"
-  //         >
-  //           <Text color="#545871" fontFamily='Roboto_400Regular'>Sign Up</Text>
-  //         </Button>
-  //       </View>
-  //     </NativeBaseProvider>
-  //   </ImageBackground>
-  // );
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVisibility, setPasswordVisibility] = useState(true);
@@ -275,8 +209,6 @@ function QuizScreen({ navigation }) {
   return <Quiz />;
 }
 
-const Drawer = createDrawerNavigator();
-
 export default function App() {
   const theme = extendTheme({
     colors: {
@@ -313,6 +245,7 @@ export default function App() {
       initialColorMode: "dark",
     },
   });
+  const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
       <Tab.Navigator
